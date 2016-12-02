3cbd40ec4ca72579066eab7cf2c0a85e399e049b
Kev <keying.li.telecom@gmail.com>
2016-12-02 12:05:34 -0500

    Add code for generating L1 itemsets

--- Apriori.py
+++ Apriori.py
@@ -13,3 +13,24 @@ DEBUG = False
 def write_output(msg):
 	with open(DIR + out_put_file_str, 'a') as f:
 		f.write(msg)
+
+
+
+def generate_L1(transactions,ts):
+	L1_cnt = dict()
+	min = ts*len(transactions)
+	for trans in transactions:
+		for item in trans:
+			if (item,) in L1_cnt:
+				L1_cnt[(item,)] += 1
+				# print(type( (item,) ))
+			else:
+				L1_cnt[(item,)] = 1
+	L1_list = []
+	for item in L1_cnt:
+		if L1_cnt[item]>= min:
+			L1_list.append(item)
+	# print('***')
+	# print(L1_list)
+	L1_list = sorted(L1_list) # sorted by the item name.
+	return L1_list, L1_cnt # not that un qualified items are not deleted yet. 
